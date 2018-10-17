22756ba7c74e9bc335bb15f6060c7804248f1d3f
Veronika Tueros <vst17@rutgers.edu>
2018-10-16 20:36:19 -0400

    creating app

--- app.py
+++ app.py
@@ -4,3 +4,18 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, desc
 from flask import Flask, jsonify, render_template, request
 import csv
+
+
+# Create App
+app = Flask(__name__)
+
+# Connect to sqlite database
+engine = create_engine("sqlite://")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+session = Session(engine)
+
+# Storing tables
+Otu = Base.classes.otu
+Samples = Base.classes.samples
+Samples_Metadata = Base.classes.samples_metadata
