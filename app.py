f7f85bc7a2e163bc2de373ff0bd32439d115caf5
Veronika Tueros <vst17@rutgers.edu>
2018-10-18 20:29:37 -0400

    sqlite and jacascript for customerIDs

--- app.py
+++ app.py
@@ -10,12 +10,36 @@ import csv
 app = Flask(__name__)
 
 # Connect to sqlite database
-engine = create_engine("sqlite://")
+engine = create_engine("sqlite:///dataset/telemarker_db.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 session = Session(engine)
 
-# Storing tables
-Otu = Base.classes.otu
-Samples = Base.classes.samples
-Samples_Metadata = Base.classes.samples_metadata
+
+# Storing tables# Storing tables
+Telemarker_db = Base.classes.telemarker_db
+# Samples = Base.classes.samples
+# Samples_Metadata = Base.classes.samples_metadata
+
+# Returns the dashboard homepage
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Returns a list of customer_ids in list format
+@app.route("/customer_ids")
+def customer_ids():
+
+    # Empty list for sample ids
+    customer_ids = []
+    
+    # Grab metadata table
+    results = session.query(Telemarker_db.customerID)
+
+    # Loop through query & grab ids
+    for result in results:
+        customer_ids.append(str(result[0]))
+
+    return jsonify(customer_ids)
+
+
