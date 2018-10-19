7cae324aa466f3ef2b6b133164d6c9ecbf28a9fc
Veronika Tueros <vst17@rutgers.edu>
2018-10-18 21:03:04 -0400

    added another route

--- app.py
+++ app.py
@@ -43,3 +43,44 @@ def customer_ids():
     return jsonify(customer_ids)
 
 
+# Returns a list of customer gender
+@app.route("/gender")
+def gender():
+
+    # Empty list for descriptions
+    customer_gender = []
+    
+    # Grab otu table
+    results = session.query(Telemarker_db.gender)
+
+    # Loop through query & grab descriptions
+    for result in results:
+        customer_gender.append(str(result[0]))
+
+    return jsonify(customer_gender)
+
+# Returns a json dictionary of sample metadata
+@app.route("/metadata/<customer>")
+def metadata(customerINFO):
+    
+    # Grab input
+    customer_info = int(customerINFO.split("_")[1])
+
+    # Empty dictionary for data
+    customer_metadata = {}
+
+    # Grab metadata table
+    customer = session.query(Telemarker_db)
+
+    # Loop through query & grab info
+    # this is where ryou need to change
+    for info in customer:
+        if (sample_id == info.SAMPLEID):
+            sample_metadata["AGE"] = info.AGE
+            sample_metadata["BBTYPE"] = info.BBTYPE
+            sample_metadata["ETHNICITY"] = info.ETHNICITY
+            sample_metadata["GENDER"] = info.GENDER
+            sample_metadata["LOCATION"] = info.LOCATION
+            sample_metadata["SAMPLEID"] = info.SAMPLEID
+
+    return jsonify(sample_metadata)
